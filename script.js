a12eb2f6d50597b4fb52118cb78cd6534c8a048b
Margherita <mangoguglia@gmail.com>
2020-06-25 18:08:19 +0200

    js-fizzbuzz

--- script.js
+++ script.js
@@ -5,12 +5,13 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
 
 for (i=1; i<=100; i++) {
+  var stampa;
 
-if(i%3==0 & i%5 == 0){
+if(i%3==0 && i%5 == 0){
 	stampa = "FizzBuzz";
 } else if (i%3 == 0) {
 	stampa = "Fizz";
-} else if (i%5==0) {
+} else if (i%5 == 0) {
 	stampa = "Buzz";
 } else {
   stampa = i;
